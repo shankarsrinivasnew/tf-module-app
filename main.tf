b8739dc284c06ee2f5a2d6bf85246e7795b62380
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-23 16:09:25 +0530

    dsfg

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_launch_template" "templater" {
+  name_prefix   = "${var.env}-${var.name}-template"
+  image_id      = data.aws_ami.ownami.image_id
+  instance_type = var.instance_type
+}
+
+resource "aws_autoscaling_group" "asgr" {
+  availability_zones = var.availability_zones
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.foobar.id
+    version = "$Latest"
+  }
+}

c017ffb7ecfe478a4e3623e9dc6363dbd563b3d2
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-28 13:14:26 +0530

    fgh

--- iam.tf
+++ iam.tf
@@ -18,7 +18,7 @@ resource "aws_iam_policy" "ssm_policy" {
           "ssm:GetParameter"
         ],
         "Resource" : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.current.account_id}:parameter/${var.env}.${var.component}*"
-        "Resource" : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.current.account_id}:parameter/${var.env}.${var.docdb}*",
+        "Resource" : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.current.account_id}:parameter/${var.env}.docdb*",
       },
       {
         "Sid" : "VisualEditor1",

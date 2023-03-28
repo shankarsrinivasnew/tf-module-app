809bbe945a49609c4048de6335d5f070c59bafc1
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-28 23:24:01 +0530

    dsfg

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   dns_env  = var.env == "prod" ? "www" : "var.env"
   dns_name = var.component == "frontend" ? "${var.env}.${var.dns_domain}" : "${var.component}-${var.env}.${var.dns_domain}"
 
-  ssm_parameters = concat([ var.component ], var.parameters)
+  ssm_parameters = concat([ var.component ], var.ssm_parameters)
 }
 
 

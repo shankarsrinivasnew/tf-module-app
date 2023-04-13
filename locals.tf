d3538d14499e00f27454e34febd92197394074ca
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-13 21:05:14 +0530

    dfg

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   dns_word  = var.env == "prod" ? "www" : var.env
   dns_name = var.component == "frontend" ? "${local.dns_word}.${var.dns_domain}" : "${var.component}-${var.env}.${var.dns_domain}"
 
-  ssm_parameters = concat([ var.component ], var.ssm_parameters)
+  ssm_parameters = concat([ var.component,"nexus" ], var.ssm_parameters)
 }
 
 
